ecc5441266fc50f9e0646400e5698e1b984aab85
raveendrau@gmail.com <raveendrau@gmail.com>
2020-04-24 11:23:51 +0530

    initial commit

---
+++ src/main/java/own/practice/angularspringboothellworldapi/AngularSpringbootHellworldApiApplication.java
@@ -0,0 +1,13 @@
+package own.practice.angularspringboothellworldapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootHellworldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootHellworldApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/own/practice/angularspringboothellworldapi/AngularSpringbootHellworldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package own.practice.angularspringboothellworldapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringbootHellworldApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
